320fcc52e1fbdf33262a9e225e8878afc1bfbc9d
manikanta5666 <69569736+manikanta5666@users.noreply.github.com>
2021-03-19 15:41:19 +0530

    Add files via upload

---
+++ lab 2.cs
@@ -0,0 +1,18 @@
+﻿namespace lab2
+{
+    public class ExExample
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
+            catch (Exception e) { Console.WriteLine(e); }
+            finally { Console.WriteLine("Finally block is executed"); }
+            Console.WriteLine("Rest of the code");
+        }
+    }
+}
