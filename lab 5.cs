bbe2d38ea675650efea24a962188d5303aa918c8
manikanta5666 <69569736+manikanta5666@users.noreply.github.com>
2021-03-19 15:52:45 +0530

    Add files via upload

---
+++ lab 5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myList = new List<string>();
+            myList.Add("1.Abdul");
+            myList.Add("2.Kiran");
+            myList.Add("3.Ashok");
+            myList.Add("4.keerthi");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+            myList.Sort();
+            Console.WriteLine("\n After sorting");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+            myList.Remove("keerthi");
+            Console.WriteLine("\n Removing of Keerthi");
+            foreach (string s in myList)
+                Console.WriteLine(s.ToString() + " ");
+            myList.Insert(2, "Thulasi");
+            Console.WriteLine("\n Inserting of Thulasi at index position 2");
+            foreach (string s in myList)
+                Console.Write(s.ToString() + " ");
+        }
+    }
+}
