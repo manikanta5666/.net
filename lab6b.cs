ae938ed2adb6fecc7ce28dd5e2f064da31cb0717
manikanta5666 <69569736+manikanta5666@users.noreply.github.com>
2021-03-19 16:23:03 +0530

    Add files via upload

---
+++ lab6b.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FileStream f = new FileStream("d:\\b.txt", FileMode.OpenOrCreate);
+
+            Console.WriteLine("File opened");
+
+            //declared stream reader
+            StreamReader sr = new StreamReader(f);
+            Console.WriteLine("Reading data from the file");
+
+            string line = sr.ReadLine();
+
+            Console.WriteLine("The data from the file is : " + line);
+
+            //closing stream writer
+            sr.Close();
+            f.Close();
+
+            Console.WriteLine("File Stream closed");
+
+            Console.ReadLine();
+        }
+    }
+}
